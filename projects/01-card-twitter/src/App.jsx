2a45e3ca4474dd060639764e79127b35f1a16298
helmerhdez <helmer0013@gmail.com>
2023-07-18 23:54:10 -0500

    Add project structure for card twitter practice

---
+++ projects/01-card-twitter/src/App.jsx
@@ -0,0 +1,16 @@
+export function App() {
+    return (
+    <article>
+        <header>
+            <img src="https://unavatar.io/duckduckgo/gummibeer.dev" alt="avatar of bear" />
+            <div>
+                <strong>Name Bear</strong>
+                <span>@bear</span>
+            </div>
+        </header>
+        <aside>
+            <button>Follow</button>
+        </aside>
+    </article>
+    )
+}
